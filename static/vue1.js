e6bd4546f2a2ec5d1383f32424f10709c628f4d0
daimon99 <daijian1@qq.com>
2016-01-20 20:37:23 +0800

    Initial commit

---
+++ static/vue1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Daimon on 2016/1/20.
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue.js' },
+            { text: 'Build Something Awesome' }
+        ]
+    }
+})
